076d4fd8e721975472e03746036b572e130039f8
Katsute <58778985+Katsute@users.noreply.github.com>
2021-01-06 13:48:06 -0500

    java

---
+++ Java/Data Structures/Java_Map.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Java_Map {
+
+    public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+
+		final Map<String,Integer> map = new HashMap<>();
+
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+			map.put(name, phone);
+			in.nextLine();
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            System.out.println(map.containsKey(s) ? s + '=' + map.get(s) : "Not found");
+		}
+	}
+
+}
